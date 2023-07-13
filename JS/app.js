f9fa1f427634f9f2a9fd966b44e06c6213d241a4
Taborda Federico <leoneltaborda3312@gmail.com>
2023-07-13 15:32:36 -0300

    Botones y Json

--- JS/app.js
+++ JS/app.js
@@ -1,18 +1,19 @@
-// La idea es generar una calculadora que te indique el consumo electrico mensual y cuanto tendrias que pagar aprox.//
-let Electrodomesticos= [
-  {id: 1, name:'Heladera', kWh: 0.035, cantidad: 1 , },
-  {id: 2, name:'A.C', kWh: 0.658,cantidad:2 , },
-  {id: 3, name:'TermoTanque', kWh: 1.5,cantidad:1 , },
-  {id: 4, name:'Microondas', kWh: 0.64,cantidad: 1,},
-  {id: 5, name:'TV', kWh: 0.09, cantidad:3 ,},
-  {id: 6, name:'Focos', kWh: 0.011, cantidad:8 ,}
-];
 
-//El resultado cambia en relacion a la cantidad de electrodomesticos que se tengan//
-var sumakWh    = 0;
-var nroElectrodomesticos = 0;
-var ConsumoMes = 0;
-var HorasUsoDiario = prompt ("Horas de uso") ;
+let Electrodomesticos= [
+  {id: 1, name:'Heladera', kWh: 0.035, cantidad: 0 , },
+  {id: 2, name:'A.C', kWh: 0.658,cantidad:0 , },
+  {id: 3, name:'TermoTanque', kWh: 1.5,cantidad:0 , },
+  {id: 4, name:'Microondas', kWh: 0.64,cantidad: 0,},
+  {id: 5, name:'TV', kWh: 0.09, cantidad:0 ,},
+  {id: 6, name:'Focos', kWh: 0.011, cantidad:0 ,}
+];
+localStorage.setItem('Electrodomesticos', JSON.stringify(Electrodomesticos));
+
+let sumakWh    = 0;
+let GastoMes   = 0;
+let nroElectrodomesticos = 0;
+let ConsumoMes = 0;
+let HorasUsoDiario = prompt ("Horas de uso") ;
 for (let i = 0; i < Electrodomesticos.length; i++) {
   nroElectrodomesticos += Electrodomesticos[i].cantidad;
 }
@@ -24,6 +25,35 @@ Electrodomesticos.forEach( function(Electrodomesticos){
  })
 console.log( ` Cant.Electrodomesticos: ${nroElectrodomesticos} la suma de kWh acumula: ${sumakWh+"kWh"} Consumo mensual : ${ConsumoMes+"kwH"} Costo Mensual Aprox.:${"$"+GastoMes}`);
 
+
+
+
+
+const tbody = document.getElementById('items');
+
+
+Electrodomesticos.forEach((electrodomestico) => {
+  const row = document.createElement('tr');
+
+  
+  const addButtonCell = document.createElement('td');
+  const addButton = document.createElement('button');
+  addButton.innerText = '+';
+  addButton.addEventListener('click', () => {
+    
+    electrodomestico.cantidad += 1;
+    dibujarTabla();
+  });
+  addButtonCell.appendChild(addButton);
+  row.appendChild(addButtonCell);
+
+  
+ 
+
+ 
+  tbody.appendChild(row);
+});
+
 function dibujarTabla() {
   const bodyTabla = document.getElementById('items');
   bodyTabla.innerHTML = '';
@@ -31,11 +61,24 @@ function dibujarTabla() {
   Electrodomesticos.forEach(function(electrodomestico) {
     bodyTabla.innerHTML += `
       <tr>
-        <th>${electrodomestico.id}</th>
+        <td>
+          <button onclick="sumarCantidad(${electrodomestico.id})">+</button>
+        </td>
+        
         <td>${electrodomestico.name}</td>
         <td>${electrodomestico.kWh}</td>
         <td>${electrodomestico.cantidad}</td>
       </tr>`;
   });
 }
-dibujarTabla()
+
+function sumarCantidad(id) {
+  Electrodomesticos.forEach(function(electrodomestico) {
+    if (electrodomestico.id === id) {
+      electrodomestico.cantidad += 1;
+    }
+  });
+  dibujarTabla();
+}
+
+dibujarTabla();
